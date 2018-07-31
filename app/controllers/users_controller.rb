61f06cf6c4731c45cfbc9e6fa911b644ac649536
Cathleen HENNINOT <cathleenhenninot@MacBook-Pro-de-Cathleen.local>
2018-07-31 20:58:12 +0200

    token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,19 @@ class UsersController < ApplicationController
 	  @user = User.new	
 	end
 
-	def create
-	  @user = User.new(user_params)
-    @user.save
-    redirect_to user_path(@user)
+	# def create
+	#   @user = User.new(user_params)
+ #    @user.save
+ #    redirect_to user_path(@user)
+ #  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :create
+    end
   end
 
   private
